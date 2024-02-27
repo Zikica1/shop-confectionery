9219fa292963a83a64e884b6768420e5d67db893
Žikica Drača <zikica.draca@gmail.com>
2024-02-27 18:44:50 +0100

    shopping-cart

--- dist/main.18dbc454.js
+++ dist/main.18dbc454.js
@@ -586,14 +586,24 @@ function render() {
     const menuBtn = document.querySelector(".hamburger");
     const nav = document.querySelector(".navigation-primary");
     const closeBtn = document.querySelector(".close-button");
+    const shopBasket = document.querySelector(".shopping-basket");
+    const shopCart = document.querySelector(".shopping-cart");
+    const shopBtnClos = document.querySelector(".shopping-cart-btn");
     function toggleAction() {
         menuBtn.classList.add("open");
         nav.classList.toggle("show");
+        shopCart.classList.remove("action");
     }
     menuBtn.addEventListener("click", toggleAction);
     closeBtn.addEventListener("click", ()=>{
         nav.classList.remove("show");
         menuBtn.classList.remove("open");
+    });
+    shopBasket.addEventListener("click", ()=>{
+        shopCart.classList.toggle("action");
+    });
+    shopBtnClos.addEventListener("click", ()=>{
+        shopCart.classList.remove("action");
     });
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,14 @@ export function render() {
   const menuBtn = document.querySelector('.hamburger');
   const nav = document.querySelector('.navigation-primary');
   const closeBtn = document.querySelector('.close-button');
+  const shopBasket = document.querySelector('.shopping-basket');
+  const shopCart = document.querySelector('.shopping-cart');
+  const shopBtnClos = document.querySelector('.shopping-cart-btn');
 
   function toggleAction() {
     menuBtn.classList.add('open');
     nav.classList.toggle('show');
+    shopCart.classList.remove('action');
   }
 
   menuBtn.addEventListener('click', toggleAction);
@@ -13,5 +17,13 @@ export function render() {
   closeBtn.addEventListener('click', () => {
     nav.classList.remove('show');
     menuBtn.classList.remove('open');
+  });
+
+  shopBasket.addEventListener('click', () => {
+    shopCart.classList.toggle('action');
+  });
+
+  shopBtnClos.addEventListener('click', () => {
+    shopCart.classList.remove('action');
   });
 }
