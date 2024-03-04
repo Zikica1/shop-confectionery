dfa468406d4ebf01fe4811cd121299a6c10492ad
Žikica Drača <zikica.draca@gmail.com>
2024-03-04 19:02:14 +0100

    hero

--- dist/index.739bf03c.js
+++ dist/index.739bf03c.js
@@ -584,6 +584,33 @@ async function load() {
     pages.render();
 }
 load();
+//header
+const tl1 = gsap.timeline();
+tl1.from(".logo", {
+    y: -50,
+    opacity: 0,
+    duration: 1
+}).from(".shopping-basket", {
+    y: -10,
+    x: 5,
+    opacity: 0,
+    duration: 1
+}, "-=0.4").from(".hamburger", {
+    y: -10,
+    x: 10,
+    opacity: 0,
+    duration: 1
+}, "-=0.5");
+//navigation
+const mm1 = gsap.matchMedia();
+mm1.add("(min-width: 910px)", ()=>{
+    gsap.from(".anim1", {
+        opacity: 0,
+        x: 100,
+        y: -20,
+        duration: 1.5
+    });
+});
 
 },{"2182b4642506a29c":"kTqFQ"}],"kTqFQ":[function(require,module,exports) {
 module.exports = require("333a34b064479e0f")(require("f05d53e69cfee187").getBundleURL("g05j8") + "main.18dbc454.js" + "?" + Date.now()).catch((err)=>{
--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,46 @@ async function load() {
 }
 
 load();
+
+//header
+
+const tl1 = gsap.timeline();
+
+tl1
+  .from('.logo', {
+    y: -50,
+    opacity: 0,
+    duration: 1,
+  })
+  .from(
+    '.shopping-basket',
+    {
+      y: -10,
+      x: 5,
+      opacity: 0,
+      duration: 1,
+    },
+    '-=0.4'
+  )
+  .from(
+    '.hamburger',
+    {
+      y: -10,
+      x: 10,
+      opacity: 0,
+      duration: 1,
+    },
+    '-=0.5'
+  );
+
+//navigation
+const mm1 = gsap.matchMedia();
+
+mm1.add('(min-width: 910px)', () => {
+  gsap.from('.anim1', {
+    opacity: 0,
+    x: 100,
+    y: -20,
+    duration: 1.5,
+  });
+});
