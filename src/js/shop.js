9eed73868b7097d82ea741c76bd039555cf74dfc
Žikica Drača <zikica.draca@gmail.com>
2024-04-29 20:39:46 +0200

    shop-category

--- src/js/shop.js
+++ src/js/shop.js
@@ -8,18 +8,50 @@ loader();
 
 const catTabs = document.querySelectorAll('.categ-item');
 const catLi = document.querySelectorAll('.category ul li');
+const catGrou = document.querySelectorAll('.grup-product');
+
+// function handleCategory(index) {
+//   if (index >= 0 && index < catTabs.length) {
+//     catTabs.forEach((tab) => {
+//       tab.classList.remove('action');
+
+//       catLi.forEach((item) => {
+//         item.classList.remove('current');
+//       });
+
+//       catGrou.forEach((grup) => {
+//         grup.classList.remove('current-cate');
+//       });
+
+//       catTabs[index].classList.add('action');
+//       catLi[index].classList.add('current');
+//       catGrou[index].classList.add('current-cate');
+//     });
+//   }
+// }
 
 function handleCategory(index) {
-  catTabs.forEach((item) => {
-    item.classList.remove('action');
+  console.log(index);
 
-    catLi.forEach((item) => {
-      item.classList.remove('current');
-    });
-
-    catTabs[index].classList.add('action');
-    catLi[index].classList.add('current');
+  catTabs.forEach((tab, i) => {
+    if (i === index) {
+      tab.classList.add('action');
+    } else {
+      tab.classList.remove('action');
+    }
   });
+
+  catLi.forEach((item) => {
+    item.classList.remove('current');
+  });
+
+  catGrou.forEach((grup) => {
+    console.log(grup);
+    grup.classList.remove('current-cate');
+  });
+
+  catLi[index].classList.add('current');
+  catGrou[index].classList.add('current-cate');
 }
 
 catTabs.forEach((tab, i) => {
